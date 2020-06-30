d565376ea02177eeb18b625aac0ac56455d4417e
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-06-30 17:08:59 -0400

    UserForm setup

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import FormUserDetails from "./FormPersonalDetails"
+import FormPersonalDetails from "./FormPersonalDetails"
+import Confirm from "./Confirm"
+import Success from "./Success"
 
 export class UserForm extends Component {
 
@@ -37,12 +41,32 @@ export class UserForm extends Component {
     render() {
         const { step } = this.state
         const { firstName, lastName, email, occupation, city, bio } = this.state
-        const values = {}
-        return (
-            <div>
+        const values = { firstName, lastName, email, occupation, city, bio }
 
-            </div>
-        )
+        switch(step){
+            case 1:
+                return (
+                    <FormUserDetails
+                        nextStep = {this.nextStep}
+                        handleChange = {this.handleChange}
+                        values = {values}
+                    
+                    />
+                )
+            case 2: 
+            return (
+                <h1>FormPersonalDetails</h1>
+            )
+            case 3: 
+            return (
+                <h1>Confirm</h1>
+            )
+            case 4: 
+            return (
+                <h1>Success</h1>
+            )
+        }
+
     }
 }
 
